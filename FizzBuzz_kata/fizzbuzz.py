8e031258dcff78aba2a60be635e1f174ca875709
Xala <alvaro.salazar@frenetic.ai>
2023-12-26 13:36:08 +0100

    Triangulation - function should return fizz when fizzbuzz with number 3

--- FizzBuzz_kata/fizzbuzz.py
+++ FizzBuzz_kata/fizzbuzz.py
@@ -1,2 +1,4 @@
 def fizzbuzz(num: int) -> str:
+    if num == 3:
+        return "fizz"
     return str(num)
--- FizzBuzz_kata/test_fizzbuzz_kata.py
+++ FizzBuzz_kata/test_fizzbuzz_kata.py
@@ -1,4 +1,5 @@
 import pytest
+
 from FizzBuzz_kata import fizzbuzz
 
 
@@ -6,3 +7,6 @@ class TestFizzBuzzShould:
     @pytest.mark.parametrize("given, expected", [(1, "1"), (2, "2"), (4, "4")])
     def test_return_as_string_when_fizzbuzz_number(self, given, expected):
         assert fizzbuzz.fizzbuzz(given) == expected
+
+    def test_return_fizz_when_fizzbuzz_number_3(self):
+        assert fizzbuzz.fizzbuzz(3) == "fizz"
