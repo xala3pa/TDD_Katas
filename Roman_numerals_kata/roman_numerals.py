f1e88c431e1f198fdc0c48660ade31344eedd449
Xala <alvaro.salazar@frenetic.ai>
2024-01-23 11:12:00 +0100

    Convert the arabic number 5 to roman V

--- Roman_numerals_kata/roman_numerals.py
+++ Roman_numerals_kata/roman_numerals.py
@@ -1,2 +1,4 @@
 def convert_to_roman(arabic_number):
+    if arabic_number == 5:
+        return "V"
     return "I" * arabic_number
--- Roman_numerals_kata/test_roman_numerals_kata.py
+++ Roman_numerals_kata/test_roman_numerals_kata.py
@@ -4,6 +4,7 @@ from Roman_numerals_kata import roman_numerals
 
 
 class TestRomanNumeralsShould:
-    @pytest.mark.parametrize("given, expected", [(1, "I"), (2, "II"), (3, "III")])
+    @pytest.mark.parametrize("given, expected", [(1, "I"), (2, "II"), (3, "III"), (5, "V")])
     def test_convert_to_roman_an_arabic_number(self, given, expected):
         assert roman_numerals.convert_to_roman(given) == expected
+
