a43f8ceed3385b33c7513d5ce7e299e259b6a1a4
Xala <alvaro.salazar@frenetic.ai>
2024-01-23 11:18:24 +0100

    Convert the arabic number 7 to roman VII

--- Roman_numerals_kata/roman_numerals.py
+++ Roman_numerals_kata/roman_numerals.py
@@ -3,4 +3,6 @@ def convert_to_roman(arabic_number):
         return "V"
     if arabic_number == 6:
         return "VI"
+    if arabic_number == 7:
+        return "VII"
     return "I" * arabic_number
