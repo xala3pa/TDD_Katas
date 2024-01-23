1388eb0cc909ff96ed600046ce70e8860734eb85
Xala <alvaro.salazar@frenetic.ai>
2024-01-23 09:56:29 +0100

    Refactor - avoid duplication

--- Roman_numerals_kata/roman_numerals.py
+++ Roman_numerals_kata/roman_numerals.py
@@ -1,6 +1,2 @@
 def convert_to_roman(arabic_number):
-    if arabic_number == 3:
-        return "III"
-    if arabic_number == 2:
-        return "II"
-    return "I"
+    return "I" * arabic_number
