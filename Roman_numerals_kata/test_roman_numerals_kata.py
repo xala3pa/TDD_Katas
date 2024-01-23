bd06b965e00f327db14aca8245c26b2055e770ab
Xala <alvaro.salazar@frenetic.ai>
2024-01-23 11:37:02 +0100

    Convert the arabic number 16 ,17, 18 to roman XVI, XVII, XVIII

--- Roman_numerals_kata/test_roman_numerals_kata.py
+++ Roman_numerals_kata/test_roman_numerals_kata.py
@@ -7,6 +7,7 @@ class TestRomanNumeralsShould:
     @pytest.mark.parametrize("given, expected",
                              [(1, "I"), (2, "II"), (3, "III"),
                               (5, "V"), (6, "VI"), (7, "VII"),
-                              (10, "X"), (11, "XI"), (12, "XII")])
+                              (10, "X"), (11, "XI"), (12, "XII"),
+                              (16, "XVI"), (17, "XVII"), (18, "XVIII")])
     def test_convert_to_roman_an_arabic_number(self, given, expected):
         assert roman_numerals.convert_to_roman(given) == expected
