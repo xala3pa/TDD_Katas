05c8b798d6e645aec9c9fe2f368a498d3fd02e5c
Xala <alvaro.salazar@frenetic.ai>
2024-01-23 12:25:43 +0100

    final solution covering all the test cases

--- Roman_numerals_kata/roman_numerals.py
+++ Roman_numerals_kata/roman_numerals.py
@@ -1,5 +1,22 @@
-def convert_to_roman(arabic_number):
-    roman_numerals = {10: "X", 5: "V", 1: "I"}
+def convert_to_roman(arabic_number: int) -> str:
+    """
+    Convert an Arabic number to its Roman numeral representation.
+
+    Args:
+    - arabic_number (int): Arabic number to convert (should be in the range of 1 to 3999).
+
+    Returns:
+    - str: Roman numeral representation.
+    """
+    if not (0 < arabic_number < 4000):
+        raise ValueError("The number must be in the range of 1 to 3999")
+
+    roman_numerals = {
+        1000: 'M', 900: 'CM', 500: 'D', 400: 'CD',
+        100: 'C', 90: 'XC', 50: 'L', 40: 'XL',
+        10: 'X', 9: 'IX', 5: 'V', 4: 'IV',
+        1: 'I'
+    }
 
     roman_number = ""
     for value, numeral in roman_numerals.items():
--- Roman_numerals_kata/test_roman_numerals_kata.py
+++ Roman_numerals_kata/test_roman_numerals_kata.py
@@ -8,6 +8,15 @@ class TestRomanNumeralsShould:
                              [(1, "I"), (2, "II"), (3, "III"),
                               (5, "V"), (6, "VI"), (7, "VII"),
                               (10, "X"), (11, "XI"), (12, "XII"),
-                              (16, "XVI"), (17, "XVII"), (18, "XVIII")])
+                              (16, "XVI"), (17, "XVII"), (18, "XVIII"),
+                              (20, "XX"), (21, "XXI"), (26, "XXVI"),
+                              (30, "XXX"), (32, "XXXII"), (37, "XXXVII"),
+                              (4, "IV"), (9, "IX"), (19, "XIX"),
+                              (40, "XL"), (90, "XC"), (800, "DCCC"),
+                              (846, "DCCCXLVI"), (1999, "MCMXCIX"), (2008, "MMVIII")])
     def test_convert_to_roman_an_arabic_number(self, given, expected):
         assert roman_numerals.convert_to_roman(given) == expected
+
+    def test_raise_exception_with_non_integer_input(self):
+        with pytest.raises(ValueError, match="The number must be in the range of 1 to 3999"):
+            roman_numerals.convert_to_roman(5000)
