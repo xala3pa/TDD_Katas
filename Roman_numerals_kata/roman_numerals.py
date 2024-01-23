bafff88874077e926c4570ff256fbae1af20e03a
Xala <alvaro.salazar@frenetic.ai>
2024-01-23 12:03:09 +0100

    Refactor - to avoid duplication

--- Roman_numerals_kata/roman_numerals.py
+++ Roman_numerals_kata/roman_numerals.py
@@ -1,10 +1,9 @@
 def convert_to_roman(arabic_number):
+    roman_numerals = {10: "X", 5: "V", 1: "I"}
+
     roman_number = ""
-    if arabic_number >= 10:
-        roman_number += "X"
-        arabic_number -= 10
-    if arabic_number >= 5:
-        roman_number += "V"
-        arabic_number -= 5
-    roman_number += "I" * arabic_number
+    for value, numeral in roman_numerals.items():
+        while arabic_number >= value:
+            roman_number += numeral
+            arabic_number -= value
     return roman_number
